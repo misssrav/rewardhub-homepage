1bcf93ff73dfa13917ee4c6ee080c2a0fe7bf145
csuftitan <csuftitan@MacBook-Air-44.lan>
2024-02-26 13:54:54 -0800

    signin with google is completed

--- App/Screen/LoginScreen/Login.js
+++ App/Screen/LoginScreen/Login.js
@@ -2,7 +2,7 @@ import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React, { useState, useEffect } from "react";
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
-//import AsyncStorage from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Login() {
   WebBrowser.maybeCompleteAuthSession();
@@ -17,6 +17,58 @@ export default function Login() {
     // webClientId: WEB_CLIENT_ID,expoClientId
   };
   const [request, response, promptAsync] = Google.useAuthRequest(config);
+
+  const [userInfo, setUserInfo] = useState(null);
+  const getUserInfo = async (token) => {
+    //absent token
+    if (!token) return;
+    //present token
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      const user = await response.json();
+      //store user information  in Asyncstorage
+      await AsyncStorage.setItem("user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      console.error(
+        "Failed to fetch user data:",
+        response.status,
+        response.statusText
+      );
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      // Attempt to retrieve user information from AsyncStorage
+      const userJSON = await AsyncStorage.getItem("user");
+
+      if (userJSON) {
+        // If user information is found in AsyncStorage, parse it and set it in the state
+        setUserInfo(JSON.parse(userJSON));
+      } else if (response?.type === "success") {
+        // If no user information is found and the response type is "success" (assuming response is defined),
+        // call getUserInfo with the access token from the response
+        getUserInfo(response.authentication.accessToken);
+      }
+    } catch (error) {
+      // Handle any errors that occur during AsyncStorage retrieval or other operations
+      console.error("Error retrieving user data from AsyncStorage:", error);
+    }
+  };
+
+  //add it to a useEffect with response as a dependency
+  useEffect(() => {
+    signInWithGoogle();
+  }, [response]);
+
+  //log the userInfo to see user details
+  console.log(JSON.stringify(userInfo));
 
   return (
     <View>
